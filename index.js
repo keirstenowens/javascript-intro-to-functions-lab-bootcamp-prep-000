5c4ca74a532d4893bcdf5bfbf1818c3092f55af3
Keirsten Owens <keirstenowens@gmail.com>
2019-05-02 01:24:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var uppercase = "HELLO!"
+var lowercase = 'hello!'
+var mixedCase = 'Hi there!'
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
